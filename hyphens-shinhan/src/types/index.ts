df51ed84b8fdf0e971b7d7a2ca56a7a995fd0867
neaimt <pas120344@gmail.com>
2026-01-23 14:22:28 +0900

    feat: OB, YB, MENTOR 역할 타입 추가 및 임시 상수 추가

--- hyphens-shinhan/src/types/index.ts
+++ hyphens-shinhan/src/types/index.ts
@@ -6,6 +6,9 @@ export type { AllRoutePath } from './routes';
 /** 바텀 네비게이션 타입 */
 export type { NavLink } from './routes';
 
+/** 사용자 역할 타입 */
+export type UserRole = 'YB' | 'OB' | 'MENTOR';
+
 /** 바텀 네비게이션 아이템 타입 */
 export interface NavItem {
     label: string;

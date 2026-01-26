32e1831524b4fb4199454eabf43a8a5ec9f82a7c
neaimt <pas120344@gmail.com>
2026-01-26 21:00:41 +0900

    feat: 아이콘 한번에 내보낼 수 있도록 파일 추가 및 바텀 네비게이션 타입 수정, 아이콘 수정

--- hyphens-shinhan/src/constants/index.ts
+++ hyphens-shinhan/src/constants/index.ts
@@ -1,35 +1,71 @@
-import { NavItem, UserRole } from '@/types';
-import { ROUTES } from './routes';
+import { NavItem, UserRole } from '@/types'
+import { ROUTES } from './routes'
 
 /** 라우트 상수 */
-export { ROUTES } from './routes';
+export { ROUTES } from './routes'
 
-/** 바텀 네비게이션 아이템 상수 
+/** 바텀 네비게이션 아이템 상수
  * 사용자 역할에 따라 바텀 네비게이션 아이템 상수를 반환합니다.
-*/
+ */
 export const NAV_ITEMS_BY_ROLE: Record<UserRole, NavItem[]> = {
   // TODO: 멘토 - 임의로 넣어둠 달라질 예정
   MENTOR: [
-    { label: '홈', href: ROUTES.HOME.MAIN, icon: 'TEMP' },
-    { label: '활동', href: ROUTES.SCHOLARSHIP.MAIN, icon: 'TEMP' },
-    { label: '커뮤니티', href: ROUTES.COMMUNITY.MAIN, icon: 'TEMP' },
-    { label: '네트워킹', href: ROUTES.NETWORK.MAIN, icon: 'TEMP' },    
-    { label: '마이페이지', href: ROUTES.MYPAGE.MAIN, icon: 'TEMP' },
+    { label: '홈', href: ROUTES.HOME.MAIN, icon: 'IconLBoldHome' },
+    { label: '활동', href: ROUTES.SCHOLARSHIP.MAIN, icon: 'IconLBoldChart2' },
+    {
+      label: '커뮤니티',
+      href: ROUTES.COMMUNITY.MAIN,
+      icon: 'IconLBoldNote2',
+    },
+    {
+      label: '네트워킹',
+      href: ROUTES.NETWORK.MAIN,
+      icon: 'IconLBoldGlobal',
+    },
+    {
+      label: '마이페이지',
+      href: ROUTES.MYPAGE.MAIN,
+      icon: 'IconLBoldFrame',
+    },
   ],
   // TODO: 졸업 장학생 - 활동 탭만 다름 임의로 넣어둠 달라질 예정
   OB: [
-    { label: '홈', href: ROUTES.HOME.MAIN, icon: 'TEMP' },
-    { label: '활동', href: ROUTES.SCHOLARSHIP.MAIN, icon: 'TEMP' },
-    { label: '커뮤니티', href: ROUTES.COMMUNITY.MAIN, icon: 'TEMP' },
-    { label: '네트워킹', href: ROUTES.NETWORK.MAIN, icon: 'TEMP' },    
-    { label: '마이페이지', href: ROUTES.MYPAGE.MAIN, icon: 'TEMP' },
+    { label: '홈', href: ROUTES.HOME.MAIN, icon: 'IconLBoldHome' },
+    { label: '활동', href: ROUTES.SCHOLARSHIP.MAIN, icon: 'IconLBoldChart2' },
+    {
+      label: '커뮤니티',
+      href: ROUTES.COMMUNITY.MAIN,
+      icon: 'IconLBoldNote2',
+    },
+    {
+      label: '네트워킹',
+      href: ROUTES.NETWORK.MAIN,
+      icon: 'IconLBoldGlobal',
+    },
+    {
+      label: '마이페이지',
+      href: ROUTES.MYPAGE.MAIN,
+      icon: 'IconLBoldFrame',
+    },
   ],
   // 기본 장학생
   YB: [
-    { label: '홈', href: ROUTES.HOME.MAIN, icon: 'TEMP' },
-    { label: '활동', href: ROUTES.SCHOLARSHIP.MAIN, icon: 'TEMP' },
-    { label: '커뮤니티', href: ROUTES.COMMUNITY.MAIN, icon: 'TEMP' },
-    { label: '네트워킹', href: ROUTES.NETWORK.MAIN, icon: 'TEMP' },    
-    { label: '마이페이지', href: ROUTES.MYPAGE.MAIN, icon: 'TEMP' },
+    { label: '홈', href: ROUTES.HOME.MAIN, icon: 'IconLBoldHome' },
+    { label: '활동', href: ROUTES.SCHOLARSHIP.MAIN, icon: 'IconLBoldChart2' },
+    {
+      label: '커뮤니티',
+      href: ROUTES.COMMUNITY.MAIN,
+      icon: 'IconLBoldNote2',
+    },
+    {
+      label: '네트워킹',
+      href: ROUTES.NETWORK.MAIN,
+      icon: 'IconLBoldGlobal',
+    },
+    {
+      label: '마이페이지',
+      href: ROUTES.MYPAGE.MAIN,
+      icon: 'IconLBoldFrame',
+    },
   ],
-};
+}

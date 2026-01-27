fa3b2e1ddc940664ae5599f489ddac122ced5317
neaimt <pas120344@gmail.com>
2026-01-27 15:41:22 +0900

    feat: Custom Header 컴포넌트 생성
    - type에 따라 타이틀 정렬 바꿀 수 있음
    - 로고 및 네비게이션 아이템 추가 가능
    - 뒤로가기 핸들러 함수 props로 보낼 수 있음
    - 넘기는 함수 없으면 디폴트는 뒤로가기임
    - 네비게이션 클릭시 발생하는 액션임

---
+++ hyphens-shinhan/src/components/common/CustomHeader.tsx
@@ -0,0 +1,94 @@
+import { HEADER_ITEMS, HEADER_NAV_ITEM_KEY } from "@/constants";
+import { Icon, IconName } from "./Icon";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+interface PropsType {
+    type?: 'Center' | 'Left';
+    title: string;
+    logo?: IconName;
+    navItem?: (typeof HEADER_ITEMS)[HEADER_NAV_ITEM_KEY];
+    onBack?: () => void;
+    onClick?: () => void;
+}
+
+/** 커스텀 헤더 컴포넌트
+ * @param type - 헤더 타입 (Center, Left)
+ * @param title - 헤더 타이틀
+ * @param logo - ? 헤더 로고
+ * @param navItem - ? 헤더 네비게이션 아이템
+ * @param onBack - ? 뒤로가기 핸들러
+ * @param onClick - ? 네비게이션 클릭 핸들러
+ * @returns 헤더 컴포넌트
+ */
+export default function CustomHeader({ type = 'Left', title, logo, navItem, onBack, onClick }: PropsType) {
+    const router = useRouter();
+
+    // 사용자가 준 onBack이 있으면 그걸 쓰고, 없으면 router.back() 실행
+    const handleBack = () => {
+        if (onBack) {
+            onBack();
+        } else {
+            router.back();
+        }
+    };
+
+    return (
+        <header className={styles.container}>
+            {/** 헤더 뒤로가기 버튼 */}
+            <button onClick={handleBack}>
+                <Icon name='IconLLineArrowLeft' className={styles.backButton} />
+            </button>
+            <div className={cn(styles.wrapper, styles[type])}>
+                {/** 헤더 타이틀 로고 */}
+                {logo && (
+                    <Icon name={logo} className={styles.logo} />
+                )}
+                {/** 헤더 타이틀 */}
+                <h1 className={styles.title}>{title}</h1>
+            </div>
+
+            { /** 헤더 네비게이션 */}
+            {navItem && (
+                <Link href={navItem.href} className={styles.navItem} onClick={onClick}>
+                    <Icon name={navItem.icon} />
+                </Link>
+            )}
+        </header>
+    );
+}
+
+const styles = {
+    container: cn(
+        'fixed top-0 left-0 right-0 z-50',
+        'flex flex-row justify-between items-center',
+        'px-4 py-3'
+    ),
+    wrapper: cn(
+        'flex flex-1 flex-row items-center',
+    ),
+    Center: cn(
+        'justify-center',
+    ),
+    Left: cn(
+        'justify-start',
+    ),
+    backButton: cn(
+        'flex items-center justify-center w-6 h-6',
+        'text-grey-9 mr-2',
+    ),
+    logo: cn(
+        'w-6 h-6 mr-1',
+    ),
+    title: cn(
+        'title-18 text-black',
+    ),
+    nav: cn(
+        'flex flex-row gap-x-4',
+    ),
+    navItem: cn(
+        'flex items-center justify-center w-6 h-6',
+        'text-grey-9',
+    ),
+}      

7ac8a7c283a3b86fd5a5350650c33b4abcf45e0c
neaimt <pas120344@gmail.com>
2026-01-28 17:04:39 +0900

    feat: 공용 탭 컴포넌트 및 커뮤니티 전용 탭 컴포넌트 생성
    - 라우터 경로 사용해서 탭 화면 불러오도록 설계

---
+++ hyphens-shinhan/src/components/common/Tab.tsx
@@ -0,0 +1,42 @@
+'use client';
+import { cn } from "@/utils/cn";
+
+interface PropsType {
+    title: string;
+    isActive: boolean;
+    onClick: () => void;
+}
+
+/**
+ * 탭 컴포넌트
+ * @param {string} title - 탭 제목
+ * @param {boolean} isActive - 탭 활성 여부
+ * @returns {React.ReactNode} 탭 컴포넌트
+ * 
+ * @example
+ * <Tab title="게시판" isActive={true} />
+ */
+export default function Tab({ isActive, title, onClick }: PropsType) {
+    return (
+        <button className={styles.container} onClick={onClick}>
+            <h1 className={styles.title(isActive)}>{title}</h1>
+            <div className={styles.line(isActive)} />
+        </button>
+    );
+}
+
+const styles = {
+    container: cn(
+        'flex flex-col items-center justify-between',
+    ),
+    title: (isActive: boolean) => cn(
+        'px-2.5 pt-2.5 pb-2',
+        'body-5 transition-colors duration-100',
+        isActive ? 'text-grey-11' : 'text-grey-6',
+    ),
+    line: (isActive: boolean) => cn(
+        'w-full h-0.5',
+        'transition-colors duration-100',
+        isActive ? 'bg-primary-shinhanblue' : 'bg-transparent',
+    ),
+};
---
+++ hyphens-shinhan/src/components/community/CommunityTabs.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { cn } from "@/utils/cn";
+import Tab from "../common/Tab";
+import { useRouter, useSearchParams } from "next/navigation";
+
+/** 커뮤니티 탭 컴포넌트 
+ * 
+ * @returns {React.ReactNode} 커뮤니티 탭 컴포넌트
+ * 게시판, 소모임, 커뮤니티 탭을 제공하며, 탭을 클릭하면 해당 탭으로 이동
+ * @example
+ * <CommunityTabs />
+ */
+export default function CommunityTabs() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const activeTab = searchParams.get('tab') || '게시판'; // URL에서 탭 정보 읽기
+
+    const handleTabClick = (title: string) => {
+        // URL을 변경함 (예: /community?tab=소모임)
+        router.push(`?tab=${title}`);
+    };
+
+    return (
+        <div className={styles.container}>
+            <Tab isActive={activeTab === '게시판'} title='게시판' onClick={() => handleTabClick('게시판')} />
+            <Tab isActive={activeTab === '소모임'} title='소모임' onClick={() => handleTabClick('소모임')} />
+            <Tab isActive={activeTab === '커뮤니티'} title='커뮤니티' onClick={() => handleTabClick('커뮤니티')} />
+        </div>
+    );
+}
+
+const styles = {
+    container: cn(
+        'flex flex-row gap-[6px]',
+        'px-4',
+    ),
+};

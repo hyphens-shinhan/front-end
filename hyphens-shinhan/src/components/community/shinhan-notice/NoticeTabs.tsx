2a799258a8946562600093b9d80cc9a5328b53b5
neaimt <pas120344@gmail.com>
2026-01-29 00:46:52 +0900

    feat: 신한장학재단 공지 NoticeTabs 컴포넌트 생성

---
+++ hyphens-shinhan/src/components/community/shinhan-notice/NoticeTabs.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { cn } from "@/utils/cn";
+import Tab from "@/components/common/Tab";
+import { useRouter, useSearchParams } from "next/navigation";
+
+/** 커뮤니티 탭 컴포넌트 
+ * 
+ * @returns {React.ReactNode} 커뮤니티 탭 컴포넌트
+ * 게시판, 소모임, 커뮤니티 탭을 제공하며, 탭을 클릭하면 해당 탭으로 이동
+ * @example
+ * <CommunityTabs />
+ */
+export default function NoticeTabs() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const activeTab = searchParams.get('tab') || '공지사항'; // URL에서 탭 정보 읽기
+
+    const handleTabClick = (title: string) => {
+        // URL을 변경함 (예: /community?tab=이벤트)
+        router.push(`?tab=${title}`);
+    };
+
+    return (
+        <div className={styles.container}>
+            <Tab tabType='full' isActive={activeTab === '공지사항'} title='공지사항' onClick={() => handleTabClick('공지사항')} />
+            <Tab tabType='full' isActive={activeTab === '이벤트'} title='이벤트' onClick={() => handleTabClick('이벤트')} />
+        </div>
+    );
+}
+
+const styles = {
+    container: cn(
+        'flex flex-row gap-2',
+        'px-4 py-2',
+    ),
+};

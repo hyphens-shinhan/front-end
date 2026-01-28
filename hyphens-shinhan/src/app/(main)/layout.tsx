2d9863f2d5007a60b0cec392dec61b3232372995
neaimt <pas120344@gmail.com>
2026-01-28 12:34:30 +0900

    feat: layout.tsx를 서버 컴포넌트로 변경, header 컴포넌트를 클라이언트 컴포넌트로 변경
    - Header 컴포넌트에서 현재 경로에 따라 자동으로 헤더 설정이 되도록 로직 변경

--- hyphens-shinhan/src/app/(main)/layout.tsx
+++ hyphens-shinhan/src/app/(main)/layout.tsx
@@ -1,26 +1,16 @@
-'use client';
-
 import BottomNav from "@/components/common/BottomNav";
-import CustomHeader from "@/components/common/CustomHeader";
 import Header from "@/components/common/Header";
 import { cn } from "@/utils/cn";
-import { getHeaderConfig } from "@/utils/header";
-import { usePathname } from "next/navigation";
 
 export default function MainLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    const pathname = usePathname();
-    const headerConfig = getHeaderConfig(pathname);
-
     return (
         <div className={styles.container}>
             {/* 상단 고정 헤더 */}
-            {headerConfig && (
-                <Header title={headerConfig.title} navItems={headerConfig.navItems} />
-            )}
+            <Header />
 
             {/* 메인 컨텐츠 영역 - 독립적인 스크롤 */}
             <main className={styles.main}>

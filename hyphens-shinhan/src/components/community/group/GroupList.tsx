bf27faa49f5aca19204bcdc52bc49f321734097f
neaimt <pas120344@gmail.com>
2026-02-02 10:48:58 +0900

    feat: 공통 PillTabs 컴포넌트 생성

--- hyphens-shinhan/src/components/community/group/GroupList.tsx
+++ hyphens-shinhan/src/components/community/group/GroupList.tsx
@@ -1,20 +1,16 @@
 import { cn } from "@/utils/cn";
+import PillTabs from "@/components/common/PillTabs";
 import Separator from "@/components/common/Separator";
 import GroupCard from "./GroupCard";
 import { POST_FAB_ITEM_KEY } from "@/constants";
 import PostFAB from "@/components/common/PostFAB";
 
+const GROUP_TABS = ['전체', '내가 참여 중인', '스터디', '봉사', '취미'];
+
 export default function GroupList() {
     return (
         <div className={styles.container}>
-            {/** TODO:카테고리 탭 - 중복 선택 가능한지 확인 필요, 컴포넌트로 뺄지는 고민 */}
-            <div className={styles.tabContainer}>
-                <p className={styles.tabItem(true)}>전체</p>
-                <p className={styles.tabItem(false)}>내가 참여 중인</p>
-                <p className={styles.tabItem(false)}>스터디</p>
-                <p className={styles.tabItem(false)}>봉사</p>
-                <p className={styles.tabItem(false)}>취미</p>
-            </div>
+            <PillTabs tabs={GROUP_TABS} activeIndex={0} />
 
             <GroupCard />
             <Separator className="mx-4" />
@@ -26,16 +22,5 @@ export default function GroupList() {
 }
 
 const styles = {
-    container: cn(
-        'flex flex-col',
-    ),
-    tabContainer: cn(
-        'flex gap-2 overflow-x-auto whitespace-nowrap scrollbar-hide',
-        'px-4 py-2 ', // 스크롤 시 여유 공간을 위해 패딩 추가
-    ),
-    tabItem: (isActive: boolean) => cn(
-        'px-5 py-1.5',
-        'body-7 text-grey-6 bg-white rounded-full border border-grey-2',
-        isActive && 'body-7 text-grey-11 bg-grey-4',
-    ),
+    container: cn('flex flex-col'),
 };

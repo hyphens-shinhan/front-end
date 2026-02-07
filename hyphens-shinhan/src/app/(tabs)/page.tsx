e037e9ffb67fc7ae721d42d25e28132c406b3270
neaimt <pas120344@gmail.com>
2026-02-07 21:21:36 +0900

    feat:프로필 카드에 QR 버튼 추가
    - 기능 추가 필요

--- hyphens-shinhan/src/app/(tabs)/page.tsx
+++ hyphens-shinhan/src/app/(tabs)/page.tsx
@@ -10,8 +10,6 @@ export default function HomePage() {
       <div className="flex min-h-full flex-1 flex-col bg-primary-lighter">
         <HomeContentCard />
       </div>
-      {/* 스크롤 끝까지 내렸을 때 그라데이션 대신 흰 배경이 보이도록 */}
-      <div className="min-h-[60vh] flex-shrink-0 bg-white" aria-hidden />
     </div>
   );
 }
--- hyphens-shinhan/src/components/home/ProfileCardWithQR.tsx
+++ hyphens-shinhan/src/components/home/ProfileCardWithQR.tsx
@@ -7,6 +7,7 @@ import ProfileSkeleton from "../mypage/ProfileSkeleton";
 import EmptyContent from "../common/EmptyContent";
 import { EMPTY_CONTENT_MESSAGES } from "@/constants";
 import { cn } from "@/utils/cn";
+import { Icon } from "../common/Icon";
 
 export default function ProfileCardWithQR() {
   const { data: profile, isLoading, error } = useMyProfile();
@@ -17,14 +18,36 @@ export default function ProfileCardWithQR() {
 
   return (
     <div className={styles.container}>
-      <ProfileCard profile={profile} isMyProfile={true} />
+      {/** 드래그 바 */}
+      <div className={styles.dragBar}></div>
+      <div className={styles.profileCardContainer}>
+        {/** QR 코드가 있는 프로필 카드 */}
+        <ProfileCard profile={profile} isMyProfile={true} />
+        {/** QR 코드 버튼 */}
+        <button type="button" className={styles.qrCodeButton}>
+          <Icon name='IconLBoldQrcode' className={styles.qrCodeButtonIcon} />
+        </button>
+      </div>
     </div>
   );
 }
 
 const styles = {
   container: cn(
-    'flex flex-col px-5 py-4.5',
+    'flex flex-col px-7 pb-5',
     'bg-primary-lighter rounded-t-[24px]',
+  ),
+  dragBar: cn(
+    'w-9 h-1.5 mx-auto my-3 bg-grey-6 rounded-full',
+  ),
+  profileCardContainer: cn(
+    'relative',
+  ),
+  qrCodeButton: cn(
+    'bg-primary-shinhanblue w-10 h-10 rounded-[16px] flex items-center justify-center',
+    'absolute top-1/2 right-0 -translate-y-1/2',
+  ),
+  qrCodeButtonIcon: cn(
+    'text-white',
   ),
 }

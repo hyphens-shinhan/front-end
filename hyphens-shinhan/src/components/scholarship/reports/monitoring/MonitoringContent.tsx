6e9b06719c9389644ba25da5c6cfd4073f108c2c
neaimt <pas120344@gmail.com>
2026-02-08 16:09:57 +0900

    feat: MonitoringContent 컴포넌트 UI 생성
    - 기능 추가 필요

--- hyphens-shinhan/src/components/scholarship/reports/monitoring/MonitoringContent.tsx
+++ hyphens-shinhan/src/components/scholarship/reports/monitoring/MonitoringContent.tsx
@@ -1,4 +1,12 @@
+'use client'
+
+import { useRef, useState } from 'react'
+import MonitoringGoalHeader from './MonitoringGoalHeader'
+import MonitoringGoalItem from './MonitoringGoalItem'
+import MonitoringPeriodNotice from './MonitoringPeriodNotice'
 import type { ReportMonth } from '@/services/reports'
+import MonitoringEvidenceUpload from './MonitoringEvidenceUpload'
+import BottomFixedButton from '@/components/common/BottomFixedButton'
 
 interface MonitoringContentProps {
   year: number
@@ -9,12 +17,50 @@ export default function MonitoringContent({
   year,
   month,
 }: MonitoringContentProps) {
+  const [goalIds, setGoalIds] = useState<number[]>([0])
+  const nextIdRef = useRef(1)
+
+  const handleAddGoal = () => {
+    setGoalIds((prev) => [...prev, nextIdRef.current++])
+  }
+
+  const handleRemoveGoal = (id: number) => {
+    setGoalIds((prev) => prev.filter((goalId) => goalId !== id))
+  }
+
   return (
-    <div className="flex flex-col py-4 px-4 pb-40">
-      <p className="text-grey-9 body-5">
-        {year}년 {month}월 학업 모니터링
-      </p>
-      {/* TODO: 학업 모니터링 상세 콘텐츠 연동 */}
+    <div className="flex flex-col py-4 pb-40">
+      {/** 상단 안내 문구 */}
+      <MonitoringPeriodNotice />
+
+      {/** 학습 목표 추가/수정 섹션 */}
+      <MonitoringGoalHeader onAddGoal={handleAddGoal} />
+
+      {/** 학습 목표 리스트 */}
+      <div className="flex flex-col">
+        {goalIds.map((id, index) => (
+          <MonitoringGoalItem
+            key={id}
+            goalIndex={index + 1}
+            onRemove={() => handleRemoveGoal(id)}
+          />
+        ))}
+      </div>
+
+      {/** 증빙자료 업로드 섹션 */}
+      <MonitoringEvidenceUpload />
+
+      {/** (저장하기, 수정하기) / (제출하기) 버튼 영역 */}
+      <BottomFixedButton
+        label='제출하기'
+        size='L'
+        type='primary'
+        onClick={() => { }}
+        secondLabel='저장하기'
+        secondType='secondary'
+        secondDisabled={false}
+        onSecondClick={() => { }}
+      />
     </div>
   )
 }

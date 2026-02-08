a96b1ba587fc6d447ae2e7466baa3177ea67cd91
neaimt <pas120344@gmail.com>
2026-02-08 16:06:40 +0900

    style: MonitoringGoalHeade 컴포넌트 스타일 수정

--- hyphens-shinhan/src/components/scholarship/reports/monitoring/MonitoringGoalHeader.tsx
+++ hyphens-shinhan/src/components/scholarship/reports/monitoring/MonitoringGoalHeader.tsx
@@ -1,5 +1,4 @@
 import Button from '@/components/common/Button'
-import { Icon } from '@/components/common/Icon'
 import { cn } from '@/utils/cn'
 import ReportTitle from '../ReportTitle'
 
@@ -13,7 +12,7 @@ export default function MonitoringGoalHeader({ onAddGoal }: MonitoringGoalHeader
     <div className={styles.container}>
       {/** 학습 목표 라벨 */}
       <div className={styles.labelWrapper}>
-        <ReportTitle title='학습 목표를 설정해주세요' checkIcon={true} />
+        <ReportTitle title='학습 목표를 설정해주세요' checkIcon={true} className='py-0' />
         <p className={styles.caption}>최소 2개의 목표가 필요해요</p>
       </div>
 
@@ -32,9 +31,6 @@ export default function MonitoringGoalHeader({ onAddGoal }: MonitoringGoalHeader
 const styles = {
   container: cn('flex px-4 py-2 justify-between'),
   labelWrapper: cn('flex flex-col gap-1.5'),
-  titleRow: cn('flex items-center gap-1.5'),
-  title: cn('title-16 text-grey-11'),
-  icon: cn('text-grey-4'),
   caption: cn('font-caption-caption4 text-grey-8'),
   button: cn('text-primary-shinhanblue'),
 }
